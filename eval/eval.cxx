75c867d8e813de2e97252c575c95dbf4df3be2d2
David Mackay <davidjx8p@gmail.com>
2015-11-17 23:27:28 +0000

    Some work on the REPL.

--- eval/eval.cxx
+++ eval/eval.cxx
@@ -1,16 +1,51 @@
-#include <stdio.h>
+#include <cstdio>
+#include <fstream>
 
 #include "eval.tab.i"
 #include "eval.tab.h"
 #include "eval.yy.h"
 
+int read (const char * text)
+{
+    /* Lex requires this scanner structure in order to operate. */
+    yyscan_t scanner;
+    /* It also requires a suitable buffer-state structure. */
+    YY_BUFFER_STATE yybuffer;
+
+    /* The scanner structure needs to be initialised. */
+    evallex_init (&scanner);
+    /* Now we need to scan our string into the buffer. */
+    yybuffer = eval_scan_string (text, scanner);
+
+    try
+    {
+        yyparse (scanner);
+    }
+    catch (int)
+    {
+        printf ("Caught error during read().\n");
+    }
+
+    /* Now we can clean up the buffer. */
+    eval_delete_buffer (yybuffer, scanner);
+    /* And, finally, destroy this scanner. */
+    evallex_destroy (scanner);
+    return 0;
+}
+
 int main (int argc, char * argv[])
 {
-    yyscan_t scanner;
-    FILE * in = fopen (argv[1], "r");
+    int returnValue = 0;
+    char buffer[256];
 
-    if (evallex_init (&scanner))
-        exit (1);
+    while (returnValue != 99)
+    {
+        std::string prompt ("{ 0 } ok ");
+        fputs (prompt.c_str (), stdout);
+        fflush (stdout);
+        if (fgets (buffer, sizeof (buffer), stdin) == NULL)
+            break;
 
-    yyparse (scanner);
+        read (buffer);
+    }
 }
