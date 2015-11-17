0f23b28ab0b7d0b06aeec1e852271c95e4bfcfe9
David Mackay <davidjx8p@gmail.com>
2015-11-17 02:15:13 +0000

    Add a minimal CMake setup.

---
+++ util/btyacc/config.h
@@ -0,0 +1,17 @@
+/* Define if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define to 1 if mkstemp() is available and working. */
+#define HAVE_MKSTEMP 1
+
+/* Define to 1 if vsnprintf() is available and working. */
+#define HAVE_VSNPRINTF 1
+
+/* Define to 1 if filesystem supports mixed-case filenames. */
+#define MIXEDCASE_FILENAMES 1
+
+/* Define to the system name. */
+#define SYSTEM_NAME "pprproc1.0"
+
+/* Define to 1 to enable backtracking extension */
+#define YYBTYACC 1
